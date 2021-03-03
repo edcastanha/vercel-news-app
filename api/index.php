741b4a2d5275ff779b84c0ff361d155d65e66a1c
Edson Lourenço <edcastanha@gmail.com>
2021-03-03 13:53:27 -0300

    TESTE VERCEL 1

--- api/index.php
+++ api/index.php
@@ -1,13 +1,16 @@
 <?php
 
+
+echo 'TESTE VERCEL'
 /*# Verificação de controle de rota
 /## Caso a view não seja encontrada retorna para index
 /
 /
-*/
+
 if(isset($_GET['view'])){
     if(file_exists('./core/controles/'.strtolower($_GET['view'])));
 
 }else{
     include('./indexControler.php');
 } 
+*/
